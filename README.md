9f3262eb670598413e5e533ff35f8c12486daa58
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-01-24 20:24:06 -0500

    card layout

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=6741457&assignment_repo_type=AssignmentRepo)
 # Program Title (edit this)
 
-Put your program description here.
+Description: Matching Game
+
+In this game, the user will flip cards when clicked and it will reveal a colour with a shape in the box and they have to find another matching card with the same colour and shape. To complete and win the game the user has to pair up all the pairs. 
+
+
+Objective of the game (what is the point)
+
+gameplay mechanics and/or user interaction
+scoring (if any)
+
+limitations (any important things your program doesn't do that is worth noting)
+
--- Sketch.java
+++ Sketch.java
@@ -5,6 +5,20 @@ public class Sketch extends PApplet {
   public int red = 225;
   public int green = 225;
   public int blue = 255;
+  public int white = 255;
+  public int black = 0;
+
+  // size variables
+  public int width = 600;
+  public int height = 600;
+
+  // card variables
+  public int cardX;
+  public int cardY = 150;
+  public int cardWidth = 90;
+  public int cardHeight = cardWidth;
+
+
 	
 	
   /**
@@ -12,7 +26,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(600, 600);
+    size(width, height);
   }
 
   /** 
@@ -20,14 +34,43 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(red, green, blue);
+    background(white);
   }
+  //array for 4 by 4 layout
+  public static String[][] board = new String[4][4];
+
+  //array to store cards
+  public static String[][] cards = new String[4][4];
+
+  //public static Scanner scanner = new Scanner(System.in);
+
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+
     //print out card layout 4 by 4 
+    stroke(black);
+    fill(black);
+    rect(cardX, cardY, cardWidth, cardHeight);
+
+    for (cardX = 90; cardX <= 225; cardX += 75) {
+      rect(cardX, cardY, cardWidth, cardHeight);
+    }
+    
+    /**for (int circleX = 75; circleX <= 225; circleX += 75) {
+      ellipse(circleX, 150, 50, 50);
+    }
+    
+    for (int circleX = 75; circleX <= 225; circleX += 75) {
+      ellipse(circleX, 225, 50, 50);
+    }
+    */
+
+    }
+  
+    
     
     
 
@@ -36,4 +79,7 @@ public class Sketch extends PApplet {
   
   // define other methods down here.
 }
-}
+
+
+
+
